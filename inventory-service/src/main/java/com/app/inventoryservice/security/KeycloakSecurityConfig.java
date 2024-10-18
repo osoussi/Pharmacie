58dc9aa704b9501ac7b407ac7e6030d8dcce94bc
Oussama Soussi <oussama.soussi.it@gmail.com>
2024-10-18 16:28:57 +0100

    add keycloak beans

---
+++ inventory-service/src/main/java/com/app/inventoryservice/security/KeycloakSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.app.inventoryservice.security;
+
+import com.app.inventoryservice.security.converter.JwtAuthConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class KeycloakSecurityConfig {
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/api/inventory/medicaments").hasAnyRole("pharmacien")
+                        .anyRequest()  // All unmatched URLs
+                        .hasRole("Admin") // Require "Admin" role for access
+                );
+        http.oauth2ResourceServer(server -> server.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter)));
+        http.sessionManagement(session -> session.sessionCreationPolicy(STATELESS));
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+}
